bb7041067741e2bb3043ed46f33b129244dc59ea
abhisingh192 <abhineetsingh192@gmail.com>
2018-04-22 00:04:24 +0530

    page replace

--- fifopagereplace.cpp
+++ fifopagereplace.cpp
@@ -1,16 +1,19 @@
+//FIFO page replacement algorithm
+//frame size is 3
+
 #include<iostream>
 using namespace std;
 
 int main() {
 cout<<"enter the number of frames"<<endl;
-int f;
+int f;                                           //number of frames
 cin>>f;
-int arr[3];
+int arr[3];                                      //array to hold the reference frame
 cout<<"enter the length of the string"<<endl;
 int l;
 cin>>l;
 
-int s[l];
+int s[l];                                        //array to hold the reference string
 cout<<"enter the string"<<endl;
 for(int i =0;i<l;i++)
 cin>>s[i];
@@ -30,36 +33,62 @@ int ch;
 
  for(int i=0;i<3;i++)
  arr[i] = -1;
+
  if(ch==1) {
   int i,j;
   cout<<endl;
 
 
 
-  for(i=0;i<3;i++){
+  int len = 0;
+  int k = 0;
+
+  for(i=0;i<l;){
+  int fla = 0;
   cout<<"checking for... "<<s[i]<<endl;
-  if(arr[i]==-1 && arr[i-1]!=s[i]){
-      arr[i] = s[i];
-      miss++;
-  }
-  else if(arr[i]==-1 && arr[i-1]==s[i])
-  hit++;
+
+  if(arr[len]==-1){                         //if arr contains -1 means that position is empty
+  for(j=0;j<3;j++){                         //checking if the arr contains the element we are are trying to put from the reference string
+    if(s[i]==arr[j]){                          // if it contains then fla is set to 1
+         fla = 1;
+         break;
+    }
 
   }
 
+  // cout<<"fla "<<fla<<endl;
+   if(!fla){
+                                                //if fla =0, means our arr did not have the element we were trying to insert
+    arr[len]=s[i];                               //therefore insert it in the arr and increment the miss by 1
+    i++;
+    len++;
+    miss++;
+    cout<<"miss"<<endl;
+    for(j=0;j<3;j++)
+     cout<<arr[j]<<" ";
+     cout<<endl<<endl;
+   }
+   else if(fla){                               // if fla is 1, means that arr already had the element we were trying to insert,
+    i++;                                       // therefore it is a hit
+    hit++;
+    cout<<"hit"<<endl;
 
- /* for(i=0;i<3;i++)
-  cout<<arr[i]<<" ";
-  cout<<endl;
+    for(j=0;j<3;j++)
+     cout<<arr[j]<<" ";
+     cout<<endl<<endl;
+   }
+
+ /*
   cout<<"number of hits are "<<hit<<endl;
   cout<<"number of misses are "<<miss<<endl;
   cout<<endl;
   cout<<endl; */
+  }
 
+  else {
 
-  int k = 0;
+                                                        //similar operations but in thiss else block, the arr is now no longer empty
 
-  for(i=3;i<l;){
     int flag = 0;
   	for(j=0;j<3;j++){
   		if(s[i]==arr[j])
@@ -68,13 +97,12 @@ int ch;
 
 
   		  if(flag == 1){
-  		        cout<<"checking for... "<<s[i]<<endl;
+  		      //  cout<<"checking for... "<<s[i]<<endl;
                 i++;
 
                 cout<<"hit"<<endl;
                 hit++;
-                cout<<"number of hits are "<<hit<<endl;
-                cout<<"number of misses are "<<miss<<endl;
+
 
                 cout<<"no page fault"<<endl;
   		  for(j=0;j<3;j++)
@@ -85,26 +113,27 @@ int ch;
   		              }
 
   		else{
-
-  		arr[k]=s[i];
-  		cout<<"checking for... "<<s[i]<<endl;
+        //cout<<" k ="<<k<<endl;
+  		arr[k]=s[i];                                           //in case of miss, we replace the arr[k] with s[i], k is initially 0.
+  		                                                        //next time we will need to replace the next element of the arr
+  		//cout<<"checking for... "<<s[i]<<endl;
   		i++;
   		k++;
 
   		cout<<"miss"<<endl;
   		miss++;
-  		cout<<"number of hits are "<<hit<<endl;
-        cout<<"number of misses are "<<miss<<endl;
 
         //cout<<"1 page fault"<<endl;
   		for(int p=0;p<3;p++)
   		    cout<<arr[p]<<" ";
   		    cout<<endl;
             cout<<endl;
-  		    if(k>2)
+  		    if(k>2)                                              // here the frame size is 3, therefore after k=2, we will have to replace arr[0] with s[i]
   		    k=0;
 
   		      }
+
+  		}
 
   		}
   		 cout<<endl;
@@ -112,9 +141,9 @@ int ch;
          cout<<"number of misses are "<<miss<<endl;
          cout<<"number of pagefaults are "<<miss<<endl;
 
-  		}
 
   		}
+}
   		// 7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1
 
 
