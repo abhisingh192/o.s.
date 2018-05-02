7c8db788aae7063126de6cce356990bda8e498c1
abhisingh192 <abhineetsingh192@gmail.com>
2018-05-02 17:04:06 +0530

    lab 1

---
+++ chainprocess.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<unistd.h>
+using namespace std;
+int main()
+{
+	int n ;
+	cout<<"parent id "<<getpid()<<endl;
+	
+	cout<<"how many child processes you want?"<<endl;
+	cin>>n;
+	
+	int i;
+	
+	for(i=0;i<n;i++)
+	{
+		int c = fork();
+		
+		if(c!=0)
+		break;
+		
+		else if(c==0)
+		{
+			cout<<"child id "<<getpid()<<endl;
+			cout<<"parent id "<<getppid()<<endl;
+		}
+	}
+	return 0;
+			
+	
+}
